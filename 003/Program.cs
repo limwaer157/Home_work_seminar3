195731ae2fd53e938ca82fdb05889fd00193168f
Denis <beznikind@gmail.com>
2023-01-21 15:40:16 +0300

    Написать программу вычисления произведения чисел от 1 до N

---
+++ 003/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Write("Число А - ");
+int numA = Int32.Parse(Console.ReadLine());
+int index = 1;
+double result = 1;
+int numB = 1;
+while (index <= numA)
+{
+    result = result * index;
+    index = index + 1;
+}
+Console.WriteLine(result);
