f0562f537881aa7fa56cdbbb16e048ae0ae1741f
Denis <beznikind@gmail.com>
2023-01-21 16:44:03 +0300

    Показать кубы чисел, заканчивающихся на четную цифру

--- 004/Program.cs
+++ 004/Program.cs
@@ -1,2 +1,20 @@
-﻿// Показать кубы чисел, заканчивающихся на четную цифру
-Console.WriteLine("Hello, World!");
+﻿//  Показать кубы чисел, заканчивающихся на четную цифру
+Console.Write("Веддите число А - ");
+int numA =Int32.Parse(Console.ReadLine());
+int numB = 1;
+int index = 1 ;
+int numC = 0;
+int numD = 0;
+
+  while (index <= numA)
+  {
+    numB = index * index * index;
+    numC = (numB % 10) % 2;
+    index = index + 1;
+    if (numC < 1) 
+    {
+        Console.WriteLine(numB);
+    }
+  }
+
+
