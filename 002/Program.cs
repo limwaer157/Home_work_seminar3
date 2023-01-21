9b79eeff3319fd2d1eb8637b7d9859953a294166
Denis <beznikind@gmail.com>
2023-01-21 17:24:54 +0300

    Подсчитать сумму цифр в числе

--- 002/Program.cs
+++ 002/Program.cs
@@ -1,2 +1,18 @@
 ﻿// Подсчитать сумму цифр в числе
-Console.WriteLine("Hello, World!");
+Console.Write("Введите число - ");
+int numA = Int32.Parse(Console.ReadLine());
+int numB = 0;
+int numC = 0;
+int result = 0;
+int index = 0; 
+while (numA > 10)
+{
+    numB = numA % 10;
+    numA = numA / 10;
+    result = numB + result ;
+    if (numA < 10)
+    {
+    result = result + numA;
+    }
+}
+Console.WriteLine(result);
